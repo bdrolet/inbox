02b03307fb73b60c73861328d9ffb98a01e8dd52
Benjamin Drolet <bdrolet@gmail.com>
2026-06-15 11:14:50 -0700

    fix: add IAM accessor binding for process CF on hf-token secret (#19)
    Co-authored-by: Ben Drolet <ben@drolet.cloud>

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -84,6 +84,12 @@ resource "google_secret_manager_secret_iam_member" "process_cf_hubspot" {
   member    = "serviceAccount:${google_service_account.process_cf.email}"
 }
 
+resource "google_secret_manager_secret_iam_member" "process_cf_hf_token" {
+  secret_id = google_secret_manager_secret.secrets["hf-token"].secret_id
+  role      = "roles/secretmanager.secretAccessor"
+  member    = "serviceAccount:${google_service_account.process_cf.email}"
+}
+
 resource "google_secret_manager_secret_iam_member" "process_cf_google_calendar" {
   for_each  = toset(["google-calendar-client-id", "google-calendar-client-secret", "google-calendar-refresh-token"])
   secret_id = google_secret_manager_secret.secrets[each.key].secret_id
